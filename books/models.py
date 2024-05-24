38197a5df964f777318c8ec07d6f1b7c848fde7e
AlvaLind <146857078+AlvaLind@users.noreply.github.com>
2024-05-24 17:37:40 +0200

    Add Books app & Book model

---
+++ books/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+
+# Create your models here.
+
+class Book(models.Model):
+    # Fields
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=50)
+    genre = models.CharField(max_length=30)
+    published_year = models.IntegerField()
+    cover_image = models.ImageField(upload_to='book_covers/', blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    slug = models.SlugField(max_length=100, unique=True, blank=True, null=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    last_updated = models.DateTimeField(auto_now=True)
+    
+    class Meta: ordering =["-created_on"]
+    
+    def __str__(self):
+        return self.title
