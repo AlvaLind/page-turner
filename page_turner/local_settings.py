c14802b3269c16c21841149ccc90f2c8c47b1107
AlvaLind <146857078+AlvaLind@users.noreply.github.com>
2024-05-28 23:33:13 +0200

    Create css.style & add base css

--- page_turner/local_settings.py
+++ page_turner/local_settings.py
@@ -62,7 +62,7 @@ ROOT_URLCONF = 'page_turner.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -130,7 +130,11 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
+# URL to use when referring to static files
 STATIC_URL = '/static/'
+
+# Specify the directory where all static files will be collected
+STATIC_ROOT = BASE_DIR / "static"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
