043e4beaeaf5f4790aa4266ee4d15bf9d4fe322b
AlvaLind <146857078+AlvaLind@users.noreply.github.com>
2024-06-04 17:16:53 +0200

    Update book_detail view to include comments

--- books/views.py
+++ books/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
+from django.contrib import messages
 from .models import Book
+from .forms import CommentForm
 
 
 
@@ -25,6 +27,29 @@ def book_detail(request, slug):
     """
     
     queryset = Book.objects.all()
-    book = get_object_or_404(queryset, slug=slug)
+    book = get_object_or_404(queryset, slug=slug)    
     
-    return render(request, "books/book_detail.html", {"book": book},)
+    comments = book.comments.all().order_by("-created_on")
+    comment_count = book.comments.filter(approved=True).count()
+    
+    if request.method == "POST":
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.user = request.user
+            comment.book = book
+            comment.save()
+            messages.add_message(
+                request, messages.SUCCESS,
+                'Your comment has been submitted and is awaiting approval'
+            )
+            
+    comment_form = CommentForm() # Empty the comment form.
+    
+    return render(request, "books/book_detail.html", {
+        "book": book,
+        "comments": comments,
+        "comment_count": comment_count,
+        "comment_form": comment_form,       
+        },
+    )
