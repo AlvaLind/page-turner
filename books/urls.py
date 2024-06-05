c12610d49aaa96070045c4bdf5ec38a091cc17cb
AlvaLind <146857078+AlvaLind@users.noreply.github.com>
2024-06-05 09:32:27 +0200

    Add Comment delete and edit URL paths

--- books/urls.py
+++ books/urls.py
@@ -4,4 +4,8 @@ from django.urls import path
 urlpatterns = [
     path('books/', views.BookList.as_view(), name='books'),
     path('<slug:slug>/', views.book_detail, name='book_detail'),
+    path('<slug:slug>/edit_comment/<int:comment_id>',
+         views.comment_edit, name='comment_edit'),
+    path('<slug:slug>/delete_comment/<int:comment_id>',
+         views.comment_delete, name='comment_delete'),
 ]
