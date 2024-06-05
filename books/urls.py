00aea12f9016499062f63dda347b449fa1655083
AlvaLind <146857078+AlvaLind@users.noreply.github.com>
2024-06-05 21:01:02 +0200

    Update URL paths

--- books/urls.py
+++ books/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 
 urlpatterns = [
     path('books/', views.BookList.as_view(), name='books'),
-    path('<slug:slug>/', views.book_detail, name='book_detail'),
-    path('<slug:slug>/edit_comment/<int:comment_id>',
+    path('books/<slug:slug>/', views.book_detail, name='book_detail'),
+    path('books/<slug:slug>/comment/edit/<int:comment_id>',
          views.comment_edit, name='comment_edit'),
-    path('<slug:slug>/delete_comment/<int:comment_id>',
+    path('books/<slug:slug>/comment/delete/<int:comment_id>',
          views.comment_delete, name='comment_delete'),
 ]
--- static/js/comments.js
+++ static/js/comments.js
@@ -1,16 +1,16 @@
 
-/* Edit button */
+// Edit button 
 const editButtons = document.getElementsByClassName("btn-edit");
 const commentText = document.getElementById("id_body");
 const commentForm = document.getElementById("commentForm");
 const submitButton = document.getElementById("submitButton");
 
-/* Delete button and confirmation */
+// Delete button and confirmation 
 const deleteModal = new bootstrap.Modal(document.getElementById("deleteModal"));
 const deleteButtons = document.getElementsByClassName("btn-delete");
 const deleteConfirm = document.getElementById("deleteConfirm");
 
-/**
+/*
 * Initializes edit functionality for the provided edit buttons.
 * 
 * For each button in the `editButtons` collection:
@@ -26,12 +26,12 @@ for (let button of editButtons) {
     let commentContent = document.getElementById(`comment${commentId}`).innerText;
     commentText.value = commentContent;
     submitButton.innerText = "Update";
-    commentForm.setAttribute("action", `edit_comment/${commentId}`);
+    commentForm.setAttribute("action", `comment/edit/${commentId}`);
   });
 }
 
 
-/**
+/*
 * Initializes deletion functionality for the provided delete buttons.
 * 
 * For each button in the `deleteButtons` collection:
@@ -44,7 +44,7 @@ for (let button of editButtons) {
 for (let button of deleteButtons) {
   button.addEventListener("click", (e) => {
     let commentId = e.target.getAttribute("data-comment_id");
-    deleteConfirm.href = `delete_comment/${commentId}`;
+    deleteConfirm.href = `comment/delete/${commentId}`;
     deleteModal.show();
   });
 }
