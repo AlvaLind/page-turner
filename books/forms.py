9b90872550281404b0a5fc98376fa233a78ae67c
AlvaLind <146857078+AlvaLind@users.noreply.github.com>
2024-06-05 21:43:40 +0200

    Create RatingForm

--- books/forms.py
+++ books/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Comment
+from .models import Comment, Rating
 
 
 class CommentForm(forms.ModelForm):
@@ -8,7 +8,29 @@ class CommentForm(forms.ModelForm):
     """
     class Meta:
         """
-        Specify the django model and order of the fields
+        Specify the Comment model and order of the fields
         """
         model = Comment
         fields = ('body',)
+        
+
+choices = [
+    (1, 'One Star'),
+    (2, 'Two Stars'),
+    (3, 'Three Stars'),
+    (4, 'Four Stars'),
+    (5, 'Five Stars')
+]
+class RatingForm(forms.ModelForm):
+    """
+    Form class for users to rate a book
+    RadioSelect widget renders the choices as star icons (radio buttons) 
+    """
+    rating = forms.ChoiceField(choices=choices, widget=forms.RadioSelect)
+    
+    class Meta:
+        """
+        Specify the Rating model and order of the fields.  
+        """
+        model = Rating
+        fields = ('rating',)
