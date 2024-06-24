b882e2732923ab6cb7b35f5cccfbcd94572bd9a0
AlvaLind <146857078+AlvaLind@users.noreply.github.com>
2024-06-24 17:32:37 +0200

    Create view and profile page html template for user profile

--- user_profile/views.py
+++ user_profile/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import update_session_auth_hash
+from django.contrib import messages
+from .forms import UserProfileForm
 
-# Create your views here
+# Create your views here.
+
+@login_required
+def profile(request):
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, instance=request.user)
+        if form.is_valid():
+            user = form.save(commit=False)
+            new_password = form.cleaned_data.get('new_password')
+            if new_password:
+                user.set_password(new_password)
+                update_session_auth_hash(request, user)
+            user.save()
+            messages.success(request, 'Your profile was successfully updated!')
+            return redirect('profile')
+        else:
+            messages.error(request, 'Please correct the error below.')
+    else:
+        form = UserProfileForm(instance=request.user)
+    return render(request, 'profile_page.html', {'form': form})
