1a5109c57d203257810ba2fb968435f401469b73
AlvaLind <146857078+AlvaLind@users.noreply.github.com>
2024-06-27 10:13:12 +0200

    Update profile view to include the status_form post request

--- user_profile/views.py
+++ user_profile/views.py
@@ -1,15 +1,14 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import update_session_auth_hash
 from django.contrib import messages
 from .forms import UserProfileForm
 from books.models import Bookshelf, Book
-
-# Create your views here.
+from books.forms import BookshelfForm
 
 @login_required
 def profile(request):
-    if request.method == 'POST':
+    if request.method == 'POST' and 'profile_form' in request.POST:
         form = UserProfileForm(request.POST, instance=request.user)
         if form.is_valid():
             user = form.save(commit=False)
@@ -25,9 +24,33 @@ def profile(request):
     else:
         form = UserProfileForm(instance=request.user)
     
+    if request.method == 'POST' and 'status_form' in request.POST:
+        book_slug = request.POST.get('book_slug')
+        status = request.POST.get('status')
+        book = get_object_or_404(Book, slug=book_slug)
+        bookshelf = get_object_or_404(Bookshelf, book=book, user=request.user)
+        bookshelf.status = status
+        bookshelf.save()
+        return redirect('profile')
+    
     bookshelf = Bookshelf.objects.filter(user=request.user)
+    status_choices = Bookshelf.STATUS_CHOICES
     books_in_bookshelf = Book.objects.filter(bookshelf__in=bookshelf)
+    
 
+    # Prepare a list of dictionaries with book and status
+    books_with_status = []
+    for entry in bookshelf:
+        books_with_status.append({
+            'book': entry.book,
+            'status': entry.get_status_display(),
+        })
     
-    
-    return render(request, 'profile_page.html', {'form': form, 'bookshelf': bookshelf, 'books_in_bookshelf': books_in_bookshelf,})
+    return render(request, 'profile_page.html', {
+        'form': form, 
+        'bookshelf': bookshelf,
+        'books_in_bookshelf': books_in_bookshelf,
+        'books_with_status': books_with_status,
+        'status_choices': status_choices,
+    })
+        
