30c3d0b8fc8a6e95a865aa6d7699e17993ccef67
AlvaLind <146857078+AlvaLind@users.noreply.github.com>
2024-06-29 12:45:18 +0200

    Add pagination to the bookshelf on the profile page

--- user_profile/views.py
+++ user_profile/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import update_session_auth_hash
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.contrib import messages
 from .forms import UserProfileForm
 from books.models import Bookshelf, Book
@@ -8,6 +9,7 @@ from books.forms import BookshelfForm
 
 @login_required
 def profile(request):
+    # Post request in profile form 
     if request.method == 'POST' and 'profile_form' in request.POST:
         form = UserProfileForm(request.POST, instance=request.user)
         if form.is_valid():
@@ -24,6 +26,7 @@ def profile(request):
     else:
         form = UserProfileForm(instance=request.user)
     
+    # Post request in status form 
     if request.method == 'POST' and 'status_form' in request.POST:
         book_slug = request.POST.get('book_slug')
         status = request.POST.get('status')
@@ -37,7 +40,6 @@ def profile(request):
     status_choices = Bookshelf.STATUS_CHOICES
     books_in_bookshelf = Book.objects.filter(bookshelf__in=bookshelf)
     
-
     # Prepare a list of dictionaries with book and status
     books_with_status = []
     for entry in bookshelf:
@@ -45,6 +47,16 @@ def profile(request):
             'book': entry.book,
             'status': entry.get_status_display(),
         })
+    
+    # Pagination
+    paginator = Paginator(books_in_bookshelf, 4)
+    page = request.GET.get('page')
+    try:
+        books_in_bookshelf = paginator.page(page)
+    except PageNotAnInteger:
+        books_in_bookshelf = paginator.page(1)
+    except EmptyPage:
+        books_in_bookshelf = paginator.page(paginator.num_pages)
     
     return render(request, 'profile_page.html', {
         'form': form, 
