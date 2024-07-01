d9d8c759ecefc81d803db2c416bc9ec926137ab5
AlvaLind <146857078+AlvaLind@users.noreply.github.com>
2024-07-01 20:07:31 +0200

    Add 404 and 500 error handler to settings and urls

--- page_turner/urls.py
+++ page_turner/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from django.views.generic import TemplateView
 from .views import handler404, handler500
 
@@ -12,7 +14,7 @@ urlpatterns = [
     path('summernote/', include('django_summernote.urls')),
     path("", include("books.urls"), name="books-urls"),
     path('profile/', include("user_profile.urls"), name="user_profile-urls"),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 handler404 = 'page_turner.views.handler404'
 handler500 = 'page_turner.views.handler500'
